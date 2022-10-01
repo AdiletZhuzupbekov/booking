bd653430146e85e3dbc90596943e8015c18c9dbd
okulist100 <adiletdoha@gmail.com>
2022-10-01 16:46:45 +0600

    6

--- src/main/java/kg/megacom/booking/controller/HomeController.java
+++ src/main/java/kg/megacom/booking/controller/HomeController.java
@@ -5,9 +5,11 @@ import kg.megacom.booking.service.HotelService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/")
+@Controller
+@RequestMapping("/")
 public class HomeController {
     private final HotelService hotelService;
 
