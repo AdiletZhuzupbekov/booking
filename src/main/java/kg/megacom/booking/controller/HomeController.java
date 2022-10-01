bbe5b57ab4afe508696c38a38b2b753a50eae40b
okulist100 <adiletdoha@gmail.com>
2022-10-01 16:30:58 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/booking/controller/HomeController.java
@@ -0,0 +1,31 @@
+package kg.megacom.booking.controller;
+
+import kg.megacom.booking.microservices.json.HotelServiceResponse;
+import kg.megacom.booking.service.HotelService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    private final HotelService hotelService;
+
+    public HomeController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("title","Booking");
+        HotelServiceResponse[] hotels = hotelService.getHotels();
+        model.addAttribute("hotels", hotels);
+        return "home";
+    }
+    @GetMapping("/findByCity")
+    public String findByCity(Model model, @RequestParam(required = false) String city){
+        HotelServiceResponse[] hotels = hotelService.findByCity(city);
+        model.addAttribute("hotels", hotels);
+        return "home";
+    }
+}
---
+++ src/main/java/kg/megacom/booking/service/impl/HotelServiceImpl.java
@@ -0,0 +1,40 @@
+package kg.megacom.booking.service.impl;
+
+import kg.megacom.booking.microservices.FileServiceFeign;
+import kg.megacom.booking.microservices.HotelServiceFeign;
+import kg.megacom.booking.microservices.json.HotelServiceResponse;
+import kg.megacom.booking.microservices.json.UrlResponse;
+import kg.megacom.booking.service.HotelService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+    private final FileServiceFeign fileServiceFeign;
+    private final HotelServiceFeign hotelServiceFeign;
+
+    public HotelServiceImpl(FileServiceFeign fileServiceFeign, HotelServiceFeign hotelServiceFeign) {
+        this.fileServiceFeign = fileServiceFeign;
+        this.hotelServiceFeign = hotelServiceFeign;
+    }
+
+    @Override
+    public UrlResponse addPhoto(MultipartFile file) {
+        return fileServiceFeign.upload(file);
+    }
+
+    @Override
+    public HotelServiceResponse[] getHotels() {
+        return hotelServiceFeign.getInfo();
+    }
+
+    @Override
+    public HotelServiceResponse[] findByCity(String city) {
+        return hotelServiceFeign.findByCity(city);
+    }
+
+    @Override
+    public HotelServiceResponse findByName(String name) {
+        return hotelServiceFeign.findByName(name);
+    }
+}
